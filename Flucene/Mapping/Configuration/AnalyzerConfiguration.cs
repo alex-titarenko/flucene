cd36cadda1cea3d3f99d49bd9a652796b283a360
Alex Titarenko <alex.sniffer@gmail.com>
2012-08-17 17:16:44 +0300

    Added xml-comments for the some classes.

--- Flucene/Mapping/Configuration/AnalyzerConfiguration.cs
+++ Flucene/Mapping/Configuration/AnalyzerConfiguration.cs
@@ -5,43 +5,75 @@ using Lucene.Net.Analysis.Standard;
 
 namespace Lucene.Net.Odm.Mapping.Configuration
 {
+    /// <summary>
+    /// Represents the analyzer configuration using fluent interface.
+    /// </summary>
+    /// <typeparam name="T">The type of part.</typeparam>
     public class AnalyzerConfiguration<T> where T : IHasAnalyzer
     {
         private readonly T _part;
 
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Lucene.Net.Odm.Mapping.Configuration.AnalyzerConfiguration{T}" /> class.
+        /// </summary>
+        /// <param name="part">A part of fluent chain.</param>
         public AnalyzerConfiguration(T part)
         {
             _part = part;
         }
 
 
+        /// <summary>
+        /// Sets the custom analyzer.
+        /// </summary>
+        /// <typeparam name="TAnalyzer">The type of analyzer.</typeparam>
+        /// <returns>part of fluent chain.</returns>
         public T Custom<TAnalyzer>()
         {
             _part.AnalyzerType = typeof(TAnalyzer);
             return _part;
         }
 
+        /// <summary>
+        /// Sets the standard analyzer.
+        /// </summary>
+        /// <returns>part of fluent chain.</returns>
         public T Standard()
         {
             return Custom<StandardAnalyzer>();
         }
 
+        /// <summary>
+        /// Sets the keyword analyzer.
+        /// </summary>
+        /// <returns>part of fluent chain.</returns>
         public T Keyword()
         {
             return Custom<KeywordAnalyzer>();
         }
 
+        /// <summary>
+        /// Sets the simple analyzer.
+        /// </summary>
+        /// <returns>part of fluent chain.</returns>
         public T Simple()
         {
             return Custom<SimpleAnalyzer>();
         }
 
+        /// <summary>
+        /// Sets the stop analyzer.
+        /// </summary>
+        /// <returns>part of fluent chain.</returns>
         public T Stop()
         {
             return Custom<StopAnalyzer>();
         }
 
+        /// <summary>
+        /// Sets the whitespace analyzer.
+        /// </summary>
+        /// <returns>part of fluent chain.</returns>
         public T Whitespace()
         {
             return Custom<WhitespaceAnalyzer>();
--- Flucene/Mapping/Configuration/EmbeddedCollectionConfiguration.cs
+++ Flucene/Mapping/Configuration/EmbeddedCollectionConfiguration.cs
@@ -9,17 +9,31 @@ using Lucene.Net.Odm.Mapping.Providers;
 
 namespace Lucene.Net.Odm.Mapping.Configuration
 {
+    /// <summary>
+    /// Represents the embedded collection configuration using fluent interface.
+    /// </summary>
+    /// <typeparam name="TChild">The type of items in the collection.</typeparam>
     public class EmbeddedCollectionConfiguration<TChild> : EmbeddedConfiguration
     {
         private Func<TChild, bool> _predicate;
         private int _itemCounts = int.MaxValue;
 
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Lucene.Net.Odm.Mapping.Configuration.EmbeddedCollectionConfiguration{T}" /> class.
+        /// </summary>
+        /// <param name="member">A member for mapping.</param>
         public EmbeddedCollectionConfiguration(Member member)
             : base(member)
         {
         }
 
 
+        /// <summary>
+        /// Sets the prefix for all fied names.
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <returns></returns>
         public new EmbeddedCollectionConfiguration<TChild> Prefix(string prefix)
         {
             _prefix = prefix;
